f4eec62f8ea4aa317ddd4d315f94aea09da96554
Robert Velasquez <rvela042@fiu.edu>
2022-09-22 23:19:26 -0400

    router info

---
+++ geektext-back/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+module.exports = router
+//here we reach 9:34 -- how we discuss with the API 
+
+//need a route to get books
+
+//need a route to get users
+
+//need a route to get all credit card info
+
+//need a route to get shipping address info
+
+//need a route to get all wishlist info
+
+//need a route to get wishlist item info 
+
+//need a route to get comments 
